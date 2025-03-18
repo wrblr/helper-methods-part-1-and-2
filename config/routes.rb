4ca9b23d76d6e9c718b28144066ae014f9f03f63
Ben Purinton <ben.purinton@gmail.com>
2025-03-18 11:47:08 -0700

    Project updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,22 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get("/", { :controller => "movies", :action => "index" })
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # Routes for the Movie resource:
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  # CREATE
+  post("/movies", { :controller => "movies", :action => "create" })
+  get("/movies/new", { :controller => "movies", :action => "new" })
+          
+  # READ
+  get("/movies", { :controller => "movies", :action => "index" })
+  get("/movies/:id", { :controller => "movies", :action => "show" })
+  
+  # UPDATE
+  patch("/movies/:id", { :controller => "movies", :action => "update" })
+  get("/movies/:id/edit", { :controller => "movies", :action => "edit" })
+  
+  # DELETE
+  delete("/movies/:id", { :controller => "movies", :action => "destroy" })
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  #------------------------------
 end
