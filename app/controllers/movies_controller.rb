0f07460456add23c7f957f2a2575bcb71b863b36
Ben Purinton <ben.purinton@gmail.com>
2025-03-19 10:33:48 -0700

    rubocop -A

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,7 +8,7 @@ class MoviesController < ApplicationController
   def index
     matching_movies = Movie.all
 
-    @list_of_movies = matching_movies.order({ :created_at => :desc })
+    @list_of_movies = matching_movies.order({ created_at: :desc })
 
     respond_to do |format|
       format.json do
@@ -16,7 +16,7 @@ class MoviesController < ApplicationController
       end
 
       format.html do
-        render({ :template => "movies/index" })
+        render({ template: "movies/index" })
       end
     end
   end
@@ -24,11 +24,11 @@ class MoviesController < ApplicationController
   def show
     the_id = params.fetch(:id)
 
-    matching_movies = Movie.where({ :id => the_id })
+    matching_movies = Movie.where({ id: the_id })
 
     @the_movie = matching_movies.first
 
-    render({ :template => "movies/show" })
+    render({ template: "movies/show" })
   end
 
   def create
@@ -38,7 +38,7 @@ class MoviesController < ApplicationController
 
     if @the_movie.valid?
       @the_movie.save
-      redirect_to("/movies", { :notice => "Movie created successfully." })
+      redirect_to("/movies", { notice: "Movie created successfully." })
     else
       render template: "movies/new"
     end
@@ -47,34 +47,34 @@ class MoviesController < ApplicationController
   def edit
     the_id = params.fetch(:id)
 
-    matching_movies = Movie.where({ :id => the_id })
+    matching_movies = Movie.where({ id: the_id })
 
     @the_movie = matching_movies.first
 
-    render({ :template => "movies/edit" })
+    render({ template: "movies/edit" })
   end
 
   def update
     the_id = params.fetch(:id)
-    the_movie = Movie.where({ :id => the_id }).first
+    the_movie = Movie.where({ id: the_id }).first
 
     the_movie.title = params.fetch("query_title")
     the_movie.description = params.fetch("query_description")
 
     if the_movie.valid?
       the_movie.save
-      redirect_to("/movies/#{the_movie.id}", { :notice => "Movie updated successfully."} )
+      redirect_to("/movies/#{the_movie.id}", { notice: "Movie updated successfully." })
     else
-      redirect_to("/movies/#{the_movie.id}", { :alert => "Movie failed to update successfully." })
+      redirect_to("/movies/#{the_movie.id}", { alert: "Movie failed to update successfully." })
     end
   end
 
   def destroy
     the_id = params.fetch(:id)
-    the_movie = Movie.where({ :id => the_id }).first
+    the_movie = Movie.where({ id: the_id }).first
 
     the_movie.destroy
 
-    redirect_to("/movies", { :notice => "Movie deleted successfully."} )
+    redirect_to("/movies", { notice: "Movie deleted successfully." })
   end
 end
