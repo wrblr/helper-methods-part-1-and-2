6e20847230309dc01437c1e472c806af746e5fbc
wrblr <wrbell@uchicago.edu>
2025-04-02 18:08:45 +0000

    Updating subhashes within params 1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -21,8 +21,8 @@ class MoviesController < ApplicationController
 
   def create
     @movie = Movie.new
-    @movie.title = params.fetch("title")
-    @movie.description = params.fetch("description")
+    @movie.title = params.fetch(:movie).fetch(:title)
+    @movie.description = params.fetch(:movie).fetch(:description)
 
     if @movie.valid?
       @movie.save
@@ -39,7 +39,7 @@ class MoviesController < ApplicationController
   def update
     @movie = Movie.find(params.fetch(:id))
 
-    @movie.title = params.fetch("title")
+    @movie.title = params.fetch(:title)
     @movie.description = params.fetch("description")
 
     if @movie.valid?
